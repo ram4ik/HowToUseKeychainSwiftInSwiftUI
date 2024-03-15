4ed587b7d381a4464e54b6c80c40cb2810caf30c
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-03-15 20:23:43 +0200

    Initial Commit

---
+++ HowToUseKeychainSwiftInSwiftUI/HowToUseKeychainSwiftInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseKeychainSwiftInSwiftUIApp.swift
+//  HowToUseKeychainSwiftInSwiftUI
+//
+//  Created by Ramill Ibragimov on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseKeychainSwiftInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
